09a844c4de9dfe95242fb398fb207df1b6dc3ed8
Vladimir Vodolazkiy <vloldemarus@gmail.com>
2021-05-06 15:38:15 +0300

    Bonjour development in progress.

--- ClassPod/LANSettings/ServiceLocator.m
+++ ClassPod/LANSettings/ServiceLocator.m
@@ -7,7 +7,24 @@
 
 #import "ServiceLocator.h"
 
-@interface ServiceLocator ()
+NSString * const VVVServiceType     =   @"_chatter._tcp.";
+NSString * const VVVserviceDomain   =   @"local.";
+
+
+@interface ServiceLocator () <NSNetServiceDelegate, NSNetServiceBrowserDelegate>
+{
+    BOOL moreComing;
+}
+
+@property (nonatomic, retain) NSNetService *service;
+@property (nonatomic, retain) NSNetServiceBrowser *browser;
+
+- (BOOL) discover;  // blocks
+- (BOOL) resolve;   // blocks
+
+- (NSMutableData *) address;
+
+
 
 @end
 
@@ -15,19 +32,51 @@
 
 + (ServiceLocator *) sharedInstance
 {
-    static ServiceLocator * _service = nil;
-    if (!_service) {
-        _service = [[ServiceLocator alloc] init];
+    static ServiceLocator * __service = nil;
+    if (!__service) {
+        __service = [[ServiceLocator alloc] init];
     }
-    return _service;
+    return __service;
 }
 
 - (instancetype) init
 {
     if (self = [super init]) {
+        self.browser = [[NSNetServiceBrowser alloc] init];
+        [self.browser setDelegate:self];
 
+        moreComing = YES;
+
+        NSLog(@"%s: discovering...", __func__);
+
+        [self.browser searchForServicesOfType:VVVServiceType
+                                 inDomain:VVVserviceDomain];
     }
     return self;
 }
+
+
+#pragma - NSNetServiceBrowser delegate
+
+- (void)netServiceBrowser:(NSNetServiceBrowser *)browser didFindDomain:(NSString *)domainString moreComing:(BOOL)moreComing
+{
+
+}
+
+- (void)netServiceBrowser:(NSNetServiceBrowser *)browser didFindService:(NSNetService *)service moreComing:(BOOL)moreComing
+{
+    
+}
+
+
+#pragma mark - NSNetService delegate
+
+- (void) netService: (NSNetService *) sender
+      didNotPublish: (NSDictionary *) errorDict
+{
+    NSLog(@"Failed to publish service - %@",sender.name);
+    NSLog(@"Error : %@", errorDict);
+}
+
 
 @end
