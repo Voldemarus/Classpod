7b3eb6ef741c1de0a6b3a3cbc60e19fc52145578
Vladimir Vodolazkiy <vloldemarus@gmail.com>
2021-06-30 18:01:51 +0300

    AudioForm implementation in progress. Daily snapshot.

--- ClassPod/UIComponents/GMAudioCellViewController.m
+++ ClassPod/UIComponents/GMAudioCellViewController.m
@@ -5,12 +5,32 @@
 //  Created by Водолазкий В.В. on 30.06.2021.
 //
 
+#import <AVFoundation/AVFoundation.h>
+#import <Accelerate/Accelerate.h>
+
+#import "GMAudioFormView.h"
+#import "UIColor+ColorHex.h"
 #import "GMAudioCellViewController.h"
 
+#define GMAudioCellTeacherBackground @"AAAAEE"
+#define GMAudioCellStudentBackground @"AAEEAA"
 
-#define GMAudioCellTeacherBackground = 
+#define GMAudioCellTeacherLine @"8888EE"
+#define GMAudioCellStudentLine @"88EE88"
 
-@interface GMAudioCellViewController ()
+#define GMAudioCellCenterLine @"999999"
+
+@interface GMAudioCellViewController () <AVAudioPlayerDelegate>
+{
+    NSMutableArray *dataArray;
+}
+
+@property (nonatomic, retain) AVAudioPlayer *player;
+@property (weak, nonatomic) IBOutlet GMAudioFormView *audioForm;
+@property (weak, nonatomic) IBOutlet UILabel *durationLabel;
+@property (weak, nonatomic) IBOutlet UIButton *playStopButton;
+
+- (IBAction)playStopClicked:(id)sender;
 
 @end
 
@@ -18,7 +38,41 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    // Init player
+}
+
+- (void) setAudio:(Audiochat *)audio
+{
+    if (_player || [_audio.uuid isEqualToString:audio.uuid] == NO) {
+        // dispose old player
+        _player = nil;
+    }
+    NSURL *url = [NSURL fileURLWithPath:audio.filename];
+    NSError *error = nil;
+    _player = [[AVAudioPlayer alloc] initWithContentsOfURL:url error:&error];
+    if (!error) {
+        [_player prepareToPlay];
+        NSTimeInterval t = _player.duration;
+        NSInteger min = t / 60;
+        NSInteger sec = t - (min * 60);
+        self.durationLabel.text = [NSString stringWithFormat:@"%02ld:%02ld",(long)min,(long)sec];
+    }
+}
+
+
+- (void) viewDidAppear:(BOOL)animated
+{
+    CALayer *l = self.view.layer;
+    if (self.owner == GMAudioCellOwnerTeacher) {
+        l.backgroundColor = [UIColor colorWithHexString:GMAudioCellTeacherBackground].CGColor;
+        self.audioForm.lineColor = [UIColor colorWithHexString:GMAudioCellTeacherLine];
+    } else {
+        l.backgroundColor = [UIColor colorWithHexString:GMAudioCellStudentBackground].CGColor;
+        self.audioForm.lineColor = [UIColor colorWithHexString:GMAudioCellTeacherLine];
+    }
+    self.audioForm.centerColor = [UIColor colorWithHexString:GMAudioCellCenterLine];
+
+    
 }
 
 /*
@@ -31,4 +85,6 @@
 }
 */
 
+- (IBAction)playStopClicked:(id)sender {
+}
 @end
