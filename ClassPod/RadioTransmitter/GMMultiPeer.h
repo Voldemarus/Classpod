395560bc20811d29873859d171fa7c9979bd9ae1
Vladimir Vodolazkiy <vloldemarus@gmail.com>
2021-07-07 15:02:45 +0300

    Multipeer development. Snapshot.

--- ClassPod/RadioTransmitter/GMMultiPeer.h
+++ ClassPod/RadioTransmitter/GMMultiPeer.h
@@ -8,6 +8,18 @@
 #import <Foundation/Foundation.h>
 #import <MultipeerConnectivity/MultipeerConnectivity.h>
 
+
+/**
+    Notification sent when list of subscribers is updated
+ */
+extern NSString * _Nonnull const GMMultipeerSubscribesUpdated;
+
+/**
+    Notification is sent when advertiser invited by remote client
+ */
+extern NSString * _Nonnull const GMMultipeerInviteAccepted;
+
+
 NS_ASSUME_NONNULL_BEGIN
 
 @interface GMMultiPeer : NSObject
@@ -22,6 +34,12 @@ NS_ASSUME_NONNULL_BEGIN
  set to YES to start adevertise lesson, and to NO - to stop advertising
  */
 @property (nonatomic) BOOL advertiseStatus;
+
+/**
+    List of the subscribers
+ */
+@property (nonatomic, retain) NSMutableSet <MCPeerID *> *subscribers;
+
 
 @end
 
--- ClassPod/RadioTransmitter/GMMultiPeer.m
+++ ClassPod/RadioTransmitter/GMMultiPeer.m
@@ -8,9 +8,13 @@
 #import "GMMultiPeer.h"
 #import "Preferences.h"
 
+NSString * const GMMultipeerSubscribesUpdated = @"GMMultipeerSubscribesUpdated";
+NSString * const GMMultipeerInviteAccepted = @"GMMultipeerInviteAccepted";
+
 NSString * const SERVICE_NAME   =   @"ClassPod-service";
 
-@interface GMMultiPeer () <MCNearbyServiceAdvertiserDelegate>
+@interface GMMultiPeer () < MCNearbyServiceAdvertiserDelegate,
+                            MCNearbyServiceBrowserDelegate>
 {
     MCSession *session;
     MCPeerID *peerID;
@@ -23,6 +27,8 @@ NSString * const SERVICE_NAME   =   @"ClassPod-service";
 @end
 
 @implementation GMMultiPeer
+
+@synthesize subscribers;
 
 - (instancetype) initWithLessonName:(NSString *)lessonName
 {
@@ -40,12 +46,15 @@ NSString * const SERVICE_NAME   =   @"ClassPod-service";
     if (self = [super init]) {
         teacherMode = NO;
         [self commonInit:aName];
+        browser = [[MCNearbyServiceBrowser alloc] initWithPeer:peerID serviceType:SERVICE_NAME];
+        NSAssert(browser, @"browser should be initialised");
     }
     return self;
 }
 
 - (void) commonInit:(NSString *)peerName
 {
+    subscribers = [[NSMutableSet alloc] initWithCapacity:7];
     peerID = [[MCPeerID alloc] initWithDisplayName:peerName];
     session = [[MCSession alloc] initWithPeer:peerID securityIdentity:nil encryptionPreference:MCEncryptionNone];
     _advertiseStatus = NO;
@@ -69,6 +78,8 @@ NSString * const SERVICE_NAME   =   @"ClassPod-service";
     }
 }
 
+#pragma mark - helpers -
+
 
 #pragma mark - Advertiser delegate methods -
 
@@ -80,7 +91,9 @@ NSString * const SERVICE_NAME   =   @"ClassPod-service";
     NSLog(@"Invite accepted from - %@",peerID.displayName);
     // Automatically accept
     invitationHandler(YES, session);
-    // TODO - add Toast to inform about new student
+    dispatch_async(dispatch_get_main_queue(), ^{
+        [[NSNotificationCenter defaultCenter] postNotificationName:GMMultipeerInviteAccepted object:peerID];
+    });
 }
 
 
@@ -88,6 +101,38 @@ NSString * const SERVICE_NAME   =   @"ClassPod-service";
 - (void)advertiser:(MCNearbyServiceAdvertiser *)advertiser didNotStartAdvertisingPeer:(NSError *)error
 {
     NSLog(@"Cannot start adverising peer - %@",[error localizedDescription]);
+}
+
+#pragma mark - Browser delegate methods -
+
+
+- (void) browser:(MCNearbyServiceBrowser *)browser
+              foundPeer:(MCPeerID *)peerID
+      withDiscoveryInfo:(nullable NSDictionary<NSString *, NSString *> *)info
+{
+    DLog(@"Peer found - %@",peerID.displayName);
+    DLog(@"Discovery info: %@",info);
+    [subscribers addObject:peerID];
+    dispatch_async(dispatch_get_main_queue(), ^{
+        [[NSNotificationCenter defaultCenter] postNotificationName:GMMultipeerSubscribesUpdated object:peerID];
+    });
+}
+
+// A nearby peer has stopped advertising. -- Lesson has been over
+- (void)browser:(MCNearbyServiceBrowser *)browser lostPeer:(MCPeerID *)peerID
+{
+    DLog(@"Advertiser stops - %@",peerID.displayName);
+    [subscribers removeObject:peerID];
+    dispatch_async(dispatch_get_main_queue(), ^{
+        [[NSNotificationCenter defaultCenter] postNotificationName:GMMultipeerSubscribesUpdated object:nil];
+    });
+}
+
+
+// Browsing did not start due to an error.
+- (void)browser:(MCNearbyServiceBrowser *)browser didNotStartBrowsingForPeers:(NSError *)error
+{
+    DLog(@"Cannot start browser - %@", [error localizedDescription]);
 }
 
 
